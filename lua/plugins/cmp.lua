959b489ebe4ff5ca38635ba96def897871e8aa00
nakulsoneji <nakulsoneji@gmail.com>
2024-05-21 00:45:58 -0700

    added pose to parsing

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -18,7 +18,6 @@ return {
         completion = {
           completeopt = "menu,menuone,noinsert",
         },
-
         mapping = cmp.mapping.preset.insert({
           ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
           ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
